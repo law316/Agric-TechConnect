fc5ecf771d917d8b5316f072c3dd28aa8a5f4ff0
Joshua <sector31619@gmail.com>
2026-02-08 16:46:09 +0100

    fixed AUTO TO IDENTITY

--- src/main/java/net/ikwa/techconnect/model/CreatorUserModel.java
+++ src/main/java/net/ikwa/techconnect/model/CreatorUserModel.java
@@ -19,7 +19,7 @@ import java.util.List;
 @Setter
 public class CreatorUserModel {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String email;
